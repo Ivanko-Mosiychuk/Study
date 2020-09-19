7dab3b9af2f16b9ff8d46ebdd07a56ffe09d849a
Ivanko-Mosiychuk <MWIvanko@gmail.com>
2020-09-19 19:18:31 +0300

    Add User class (name,age,email var)

---
+++ src/main/java/com/app/User.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class User {
+    private String name;
+    private String email;
+    private int age;
+
+    public User(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+}
