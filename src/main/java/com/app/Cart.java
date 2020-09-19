52550280820eab481da68468cec399d55105ca9c
Ivanko-Mosiychuk <MWIvanko@gmail.com>
2020-09-19 23:04:48 +0300

    Add items list to Cart class.

--- src/main/java/com/app/Cart.java
+++ src/main/java/com/app/Cart.java
@@ -1,4 +1,11 @@
 package com.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart {
+    private List<Item> listOfItems;
+    public List<Item> getListOfItems() {
+        return listOfItems;
+    }
 }
