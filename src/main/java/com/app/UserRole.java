819fc9eef4a58c23d78be3c4f1eb6df67e79ba28
Ivanko-Mosiychuk <MWIvanko@gmail.com>
2020-09-20 15:00:56 +0300

    Add UserRole enum B_6

---
+++ src/main/java/com/app/UserRole.java
@@ -0,0 +1,8 @@
+package com.app;
+
+public enum UserRole {
+    Admin;
+    private final String ADMIN = "admin";
+    private final String BUYER = "buyer";
+    private final String SELLER = "seller";
+}
